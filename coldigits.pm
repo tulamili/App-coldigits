e54ad13a834de2fab3ab43332edb6560d19afa47
Toshiyuki SHIMONO <t.shimono@yamanashi.ac.jp>
2024-11-18 23:27:32 +0900

    5個のファイル。coldigits はadd していない。 バージョン0.010 とする。

---
+++ coldigits.pm
@@ -0,0 +1,25 @@
+package App::coldigits ;  
+our $VERSION = '0.010' ; 
+our $DATE = '2024-03-16T13:09+09:00' ; 
+
+=encoding utf8
+
+=head1 NAME
+
+App::coldigits - TSVファイルの各列が何桁のものが何件あったかを、行列状に示す。
+
+=head1 SYNOPSIS
+
+This module provides a Unix-like command `F<coldigits>'. 
+
+=head1 DESCRIPTION
+
+=head1 SEE ALSO
+
+coldigits --help
+man coldigits
+perldoc coldigits
+
+=cut
+
+1 ;
