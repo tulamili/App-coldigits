29f2fab4ec35cc8d300f7a4bb43dd3a8d6b9eef8
Toshiyuki SHIMONO <t.shimono@yamanashi.ac.jp>
2024-11-23 14:49:35 +0900

    coldigits.pm :  coldigits とあるべき部分が全てzerogap だったので訂正。

--- coldigits.pm
+++ coldigits.pm
@@ -1,4 +1,4 @@
-package App::zerogap ;  
+package App::coldigits ;  
 our $VERSION = '0.014' ; 
 our $DATE = '2024-11-23T01:29+09:00' ; 
 
@@ -6,19 +6,19 @@ our $DATE = '2024-11-23T01:29+09:00' ;
 
 =head1 NAME
 
-App::zerogap - TSVファイルの各列が何桁のものが何件あったかを、行列状に示す。
+App::coldigits - TSVファイルの各列が何桁のものが何件あったかを、行列状に示す。
 
 =head1 SYNOPSIS
 
-This module provides a Unix-like command `F<zerogap>'. 
+This module provides a Unix-like command `F<coldigits>'. 
 
 =head1 DESCRIPTION
 
 =head1 SEE ALSO
 
-zerogap --help
-man zerogap
-perldoc zerogap
+coldigits --help
+man coldigits
+perldoc coldigits
 
 =cut
 
