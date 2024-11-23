c0dd643944c846ee314346b189bdecaf4f8e531c
Toshiyuki SHIMONO <t.shimono@yamanashi.ac.jp>
2024-11-23 20:15:03 +0900

    Changes と coldigits.pm : 0.016 であることを書いた。

--- coldigits.pm
+++ coldigits.pm
@@ -1,6 +1,6 @@
 package App::coldigits ;  
-our $VERSION = '0.014' ; 
-our $DATE = '2024-11-23T01:29+09:00' ; 
+our $VERSION = '0.016' ; 
+our $DATE = '2024-11-23T20:00+09:00' ; 
 
 =encoding utf8
 
